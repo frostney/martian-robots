e7392afa635ff9c65da03170d52501e7889fe2c3
Johannes Stein <johannesstein@freeze-dev.com>
2020-08-16 22:35:02 +0100

    :art:   Code formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {readFileSync} = require('fs');
+const { readFileSync } = require('fs');
 const importJsx = require('import-jsx');
 
 const parseFile = require('./parseFile');
@@ -7,15 +7,15 @@ const arg = process.argv[2];
 let filename;
 
 if (!arg) {
-    filename = 'sample.txt';
+  filename = 'sample.txt';
 }
 
 try {
-    let content = readFileSync(filename).toString();
+  const content = readFileSync(filename).toString();
 
-    parseFile(content);
+  parseFile(content);
 } catch (err) {
-    console.error(`Cannot read file: ${err}`);
+  console.error(`Cannot read file: ${err}`);
 }
 
 importJsx('./MartianRobot.jsx');
--- parseFile.js
+++ parseFile.js
@@ -1,45 +1,55 @@
 const { GRID_MAX_WIDTH, GRID_MAX_HEIGHT } = require('./constants');
 
-const splitCoordinate = (coordinate) => coordinate.split(' ')
+const splitCoordinate = (coordinate) => coordinate.split(' ');
 
 const parseFile = (content) => {
-    const lines = content.split('\n');
+  const lines = content.split('\n');
 
-    const [gridWidth, gridHeight] = splitCoordinate(lines[0]);
-    let robots = [];
+  const [gridWidth, gridHeight] = splitCoordinate(lines[0]);
+  const robots = [];
 
-    if (gridWidth > GRID_MAX_WIDTH) {
-        throw new Error(`Grid width ${gridWidth} is higher than maximum.`);
-    }
+  if (gridWidth > GRID_MAX_WIDTH) {
+    throw new Error(`Grid width ${gridWidth} is higher than maximum.`);
+  }
 
-    if (gridHeight > GRID_MAX_HEIGHT) {
-        throw new Error(`Grid height ${gridHeight} is higher than maximum.`);
-    }
+  if (gridHeight > GRID_MAX_HEIGHT) {
+    throw new Error(`Grid height ${gridHeight} is higher than maximum.`);
+  }
 
-    // Remove first line
-    lines.shift();
+  // Remove first line
+  lines.shift();
 
-    for (let i = 0; i < lines.length - 1; i += 2) {
-        const [x, y, direction] = splitCoordinate(lines[i]);
-        const actions = lines[i + 1].split('').map(action => {
-            switch (action) {
-                case 'L': return 'LEFT';
-                case 'R': return 'RIGHT';
-                case 'F': return 'FORWARD';
-                default: return;
-            }
-        });
+  for (let i = 0; i < lines.length - 1; i += 2) {
+    const [x, y, direction] = splitCoordinate(lines[i]);
+    const actions = lines[i + 1]
+      .split('')
+      .map((action) => {
+        switch (action) {
+          case 'L':
+            return 'LEFT';
+          case 'R':
+            return 'RIGHT';
+          case 'F':
+            return 'FORWARD';
+          default:
+            return '';
+        }
+      })
+      .filter((item) => item);
 
-        robots.push({
-            x, y, direction, actions
-        })
-    }
+    robots.push({
+      x,
+      y,
+      direction,
+      actions,
+    });
+  }
 
-    return {
-        gridWidth,
-        gridHeight,
-        robots
-    };
-}
+  return {
+    gridWidth,
+    gridHeight,
+    robots,
+  };
+};
 
 module.exports = parseFile;
