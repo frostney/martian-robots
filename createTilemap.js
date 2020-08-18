76434258c3ef884a010052df065e9c061161d302
Johannes Stein <johannesstein@freeze-dev.com>
2020-08-18 12:16:58 +0100

    :recycle:   Refactor tilemap creation

--- createTilemap.js
+++ createTilemap.js
@@ -1,14 +1,14 @@
 const { EMPTY, NONE } = require('./constants');
 
 const createTilemap = (width, height) => {
-  return [...Array(width)].map((x) =>
-    [...Array(height)].map((y) => ({
-      x,
-      y,
-      background: EMPTY,
-      robot: NONE,
-    })),
-  );
+  return [
+    ...Array(width).fill([
+      ...Array(height).fill({
+        background: EMPTY,
+        robot: NONE,
+      }),
+    ]),
+  ];
 };
 
 module.exports = createTilemap;
